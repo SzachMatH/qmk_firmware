940933bf71ce6a514d14f6813820afe26c0c418f
SzachMatH <szach.bart@gmail.com>
2025-11-23 19:35:56 +0100

    some minor fixes

--- keyboards/lily58/keymaps/Szachu_lily58/keymap.c
+++ keyboards/lily58/keymaps/Szachu_lily58/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LCTL,  KC_1     , KC_2      ,    KC_3   ,KC_4,    KC_5,                     KC_6,    KC_7,    KC_8       ,KC_9          ,  KC_0         , KC_GRV,
   KC_TAB ,  KC_Q     , KC_W      ,    KC_E   ,KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I       ,KC_O          ,  KC_P         , KC_MINS,
   KC_ESC ,  KC_A     , KC_S      ,    KC_D   ,KC_F,    KC_G,                     KC_H,    KC_J,    KC_K       ,KC_L          ,  KC_SCLN      , KC_BSPC,
-  KC_LSFT,ALT_T(KC_X),CTL_T(KC_X),SFT_T(KC_C),KC_V,    KC_B, OS_LCTL,  OS_RALT,  KC_N,    KC_M,RSFT_T(KC_COMM),RCTL_T(KC_DOT),RALT_T(KC_SLSH), KC_ESC ,
+  KC_LSFT,ALT_T(KC_Z),CTL_T(KC_X),SFT_T(KC_C),KC_V,    KC_B, OS_LCTL,  OS_RALT,  KC_N,    KC_M,RSFT_T(KC_COMM),RCTL_T(KC_DOT),RALT_T(KC_SLSH), KC_ESC ,
                         KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_ALGR, KC_ALGR
 ),
 /* LOWER
