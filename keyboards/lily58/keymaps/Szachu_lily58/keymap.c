f588c9114a509d280328e55da2d2ebc1ee4c09d7
SzachMatH <szach.bart@gmail.com>
2025-12-16 12:23:14 +0100

    added permissive hold

--- keyboards/lily58/keymaps/Szachu_lily58/keymap.c
+++ keyboards/lily58/keymaps/Szachu_lily58/keymap.c
@@ -15,11 +15,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |ALT(A)|CRL(S)|SHF(D)|   F  |   G  |-------.    ,-------|   H  |   J  |SHF(K)|CRL(L)|ALT(;)|BackSp|
+ * | ESC  |ALT(A)|   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |ALT(;)|BackSp|
  * |------+------+------+------+------+------|OS_LCTL|    |OS_RALT|------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | ESC  |
+ * |LShift|   Z  |CRL(X)|SHF(C)|   V  |   B  |-------|    |-------|   N  |   M  |SHF(,)|CRL(.)|   /  | ESC  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |AlrGr | AltGr|
+ *                   | LAlt | LGUI |Space | /LOWER  /       \RAISE \  |Enter |AlrGr | AltGr|
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -32,9 +32,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   KC_LCTL,  KC_1     , KC_2      ,    KC_3   ,KC_4,    KC_5,                     KC_6,    KC_7,    KC_8       ,KC_9          ,  KC_0         , KC_GRV,
   KC_TAB ,  KC_Q     , KC_W      ,    KC_E   ,KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I       ,KC_O          ,  KC_P         , KC_MINS,
-  KC_ESC ,  KC_A     , KC_S      ,    KC_D   ,KC_F,    KC_G,                     KC_H,    KC_J,    KC_K       ,KC_L          ,  KC_SCLN      , KC_BSPC,
-  KC_LSFT,ALT_T(KC_Z),CTL_T(KC_X),SFT_T(KC_C),KC_V,    KC_B, OS_LCTL,  OS_RALT,  KC_N,    KC_M,RSFT_T(KC_COMM),RCTL_T(KC_DOT),RALT_T(KC_SLSH), KC_ESC ,
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_ALGR, KC_ALGR
+  KC_ESC ,ALT_T(KC_A), KC_S      ,    KC_D   ,KC_F,    KC_G,                     KC_H,    KC_J,    KC_K       ,KC_L          ,RALT_T(KC_SCLN), KC_BSPC,
+  KC_LSFT,  KC_Z     ,CTL_T(KC_X),SFT_T(KC_C),KC_V,    KC_B, OS_LCTL,  OS_RALT,  KC_N,    KC_M,RSFT_T(KC_COMM),RCTL_T(KC_DOT),  KC_SLSH      , KC_ESC ,
+                        KC_LALT, KC_LGUI, KC_SPC, MO(_LOWER), MO(_RAISE), KC_ENT, KC_ALGR, KC_ALGR
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|OS_LCTL|    |OS_RALT|------+------+------+------+------+------|
  * |LShift|      |      |      |   [  |  ]   |-------|    |-------|   `  |   )  |   |  |   _  |   \  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| AlrGr|
+ *                   | LAlt | LGUI |Space | /LOWER  /       \RAISE \  |Enter |BackSP| AlrGr|
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -65,9 +65,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LCTRL |  F2  |  F3  |  F4  |LButn |RButn |-------.    ,-------| Left | Down |  Up  |Right |      |      |
  * |------+------+------+------+------+------|OS_LCTL|    |OS_RALT|------+------+------+------+------+------|
- * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|      |      |      |      |      |      |
+ * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| AlrGr|
+ *                   | LAlt | LGUI |Space | /LOWER  /       \RAISE \  |Enter |BackSP| AlrGr|
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
   KC_LCTL, KC_F2,   KC_F3,   KC_F4,  MS_BTN1,MS_BTN2,                       KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  _______, _______, _______, _______, _______, _______,
+  _______,_______ ,_______,  _______, _______, _______,   _______, _______,  _______, _______, _______, _______, _______, _______,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
 ),
 /* ADJUST
